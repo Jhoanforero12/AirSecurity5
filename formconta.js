3a6172b0ced4594da19486d791d1cd7c97df80f6
Jhoan Forero <jhoanforero2000@gmail.com>
2025-03-12 16:48:39 -0500

    Actualizar formconta.js

--- formconta.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    alert('¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.');
-    this.reset();
-});
